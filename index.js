82c6ed3ff5cc779be765206a06b3b7e3ab27210c
Rahul Soni <rahul.soni3883@gmail.com>
2020-05-10 19:24:33 +0530

    ...

--- index.js
+++ index.js
@@ -3,74 +3,74 @@ const util = require('util');
 let char, len, params, indent, replacer, removeQuotes, singleLineArray;
 
 function configure(options = {}) {
-    ({
-        char = '-',
-        len = 67,
-        indent = 4,
-        removeQuotes = true,
-        singleLineArray = true,
-        replacer = (key, value) => {
-            if (singleLineArray && value instanceof Array && typeof value[0] !== 'object')
-                return JSON.stringify(value);
-            return value;
-        },
-        ...params
-    } = options);
-    params.breakLength = params.breakLength || len;
+  ({
+    char = '-',
+    len = 67,
+    indent = 4,
+    removeQuotes = true,
+    singleLineArray = true,
+    replacer = (key, value) => {
+      if (singleLineArray && value instanceof Array && typeof value[0] !== 'object')
+        return JSON.stringify(value);
+      return value;
+    },
+    ...params
+  } = options);
+  params.breakLength = params.breakLength || len;
 }
 
 configure();
 
 function getString(obj, options = {}) {
-    return util.inspect(obj, {...params, ...options});
+  return util.inspect(obj, {...params, ...options});
 }
 
 function log(obj, options) {
-    console.log(getString(obj, options));
+  console.log(getString(obj, options));
 }
 
 function logC(obj, options) {
-    addLine(log, obj, options);
+  addLine(log, obj, options);
 }
 
 function getJsonString(obj, {
-    replacer:localReplacer = replacer,
-    indent:localIndent = indent,
-    removeQuotes:localRemoveQuotes = removeQuotes,
-    singleLineArray:localSingleLineArray = singleLineArray
+  replacer: localReplacer = replacer,
+  indent: localIndent = indent,
+  removeQuotes: localRemoveQuotes = removeQuotes,
+  singleLineArray: localSingleLineArray = singleLineArray
 } = {}) {
-    singleLineArray = localSingleLineArray;
-    let s = JSON.stringify(obj, localReplacer, localIndent);
-    if (localRemoveQuotes) {
-        s = s.replace(/"(\w+)"\s*:/g, '$1:');
-    }
-    return s;
+  singleLineArray = localSingleLineArray;
+  let s = JSON.stringify(obj, localReplacer, localIndent);
+  if (localRemoveQuotes) {
+    s = s.replace(/"(\w+)"\s*:/g, '$1:');
+  }
+  return s;
 }
 
 function json(obj, options) {
-    console.log(getJsonString(obj, options));
+  console.log(getJsonString(obj, options));
 }
 
 function jsonC(obj, options) {
-    addLine(json, obj, options);
+  addLine(json, obj, options);
 }
 
 function addLine(fn, obj, options) {
-    line(options);
-    fn(obj, options);
-    line(options);
+  line(options);
+  fn(obj, options);
+  line(options);
 }
 
 function line(options) {
-    console.log(getLine(options));
+  console.log(getLine(options));
 }
 
-function getLine({ char:localChar = char, len:localLen = len } = {}) {
-    let s = '';
-    for (let i = 0; i < localLen; i++) {
-        s += localChar;
-    }
-    return s;
+function getLine({char: localChar = char, len: localLen = len} = {}) {
+  let s = '';
+  for (let i = 0; i < localLen; i++) {
+    s += localChar;
+  }
+  return s;
 }
 
-module.exports = { configure, getLine, line, getString, log, logC, getJsonString, json, jsonC };
+module.exports = {configure, getLine, line, getString, log, logC, getJsonString, json, jsonC};
--- samples/sample.js
+++ samples/sample.js
@@ -1,34 +1,34 @@
 var utils = require('@rahul171/utils');
 
 utils.configure({
-    showHidden: true,
-    colors: true,
-    breakLength: 50,
-    char: '=',
-    removeQuotes: false
+  showHidden: true,
+  colors: true,
+  breakLength: 50,
+  char: '=',
+  removeQuotes: false
 });
 
 const obj = {
-    a: 1,
-    b: 'hello',
-    c: [1, 2, 3, 4],
-    d: ['hi', 'there'],
-    e: {
-        f: [11, 22, 33, 44],
-        g: 'inside'
-    }
+  a: 1,
+  b: 'hello',
+  c: [1, 2, 3, 4],
+  d: ['hi', 'there'],
+  e: {
+    f: [11, 22, 33, 44],
+    g: 'inside'
+  }
 };
 
 console.log(utils.getLine());
 
-utils.line({ char: '~' });
+utils.line({char: '~'});
 
 console.log(utils.getString(obj));
 
-utils.log(obj, { colors: false });
-utils.logC(obj, { char: '+'});
+utils.log(obj, {colors: false});
+utils.logC(obj, {char: '+'});
 
-console.log(utils.getJsonString(obj, { indent: '--', removeQuotes: true }));
+console.log(utils.getJsonString(obj, {indent: '--', removeQuotes: true}));
 
 utils.json(obj);
-utils.jsonC(obj, { singleLineArray: false });
+utils.jsonC(obj, {singleLineArray: false});
